ae000c490bc9aaf66d3cc05e0c7ad5fe3f529f0c
Serge Danson Ndekezi <sergedanson@danson-mac.local>
2021-05-23 11:01:49 +0200

    validate mint role owner, pass mint role, minting

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -4,18 +4,26 @@ pragma solidity >=0.6.0 <0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+    address public minter;
 
-  //add minter changed event
+    //add minter changed event
+    event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    constructor() public payable ERC20("Your Decentralized Bank", "Sergivy") {
+        minter = msg.sender;
+    }
 
-  //Add pass minter role function
+    function passMintRole(address dBank) public returns (bool) {
+        require(minter == msg.sender, "Only owner can pass the minter role.");
+        minter = dBank;
+        emit MinterChanged(msg.sender, dBank);
+        return true;
+    }
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
-	}
+    function mint(address account, uint256 amount) public {
+        //check if msg.sender have minter role
+
+        require(minter == msg.sender, "msg.sender doesn't have minter role");
+        _mint(account, amount);
+    }
 }
